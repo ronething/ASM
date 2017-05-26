179ca4991ad4878e993f9664975421c860cfcd5e
ashing <axingfly@gmail.com>
2017-05-26 22:21:41 +0800

    add demo
    实验

---
+++ 测试样例1.asm
@@ -0,0 +1,60 @@
+﻿DATAS SEGMENT
+    ;此处输入数据段代码  
+    string db 'BASED ADDRESSING','$'
+DATAS ENDS
+
+STACKS SEGMENT
+    ;此处输入堆栈段代码
+    dw 20h dup(?)
+    top label word
+STACKS ENDS
+
+CODES SEGMENT
+    ASSUME CS:CODES,DS:DATAS,SS:STACKS
+    
+	p proc far 
+    MOV AX,DATAS
+    MOV DS,AX
+    
+    mov ax,stacks
+    
+    mov ss,ax;堆栈段地址
+    
+    lea sp,top;堆栈偏移地址
+    
+     ;cx计数字符个数,si定位到最后一个字符
+    lea si,string
+    mov cx,0
+L1:
+	;比较是否到字符串尾
+	
+	cmp BYTE PTR [si],"$"
+	je input
+	inc si
+	inc cx
+	jmp L1
+	
+input:
+	;指向最后一个字符
+	
+	lea si,string
+	add si,cx
+	dec si
+L2:
+	;从后往前输出字符
+
+	mov dl,[si]
+	mov ah,02h
+	int 21h
+	dec si
+	loop L2
+	
+exit:
+	;返回终止码
+    
+    MOV AH,4CH
+    INT 21H
+    p endp
+    
+CODES ENDS
+    END p
---
+++ 测试样例2.asm
@@ -0,0 +1,34 @@
+﻿DATAS SEGMENT
+    ;此处输入数据段代码  
+    
+DATAS ENDS
+
+STACKS SEGMENT
+    ;此处输入堆栈段代码
+STACKS ENDS
+
+CODES SEGMENT
+    ASSUME CS:CODES,DS:DATAS,SS:STACKS
+START:
+    MOV AX,DATAS
+    MOV DS,AX
+    ;此处输入代码段代码
+    mov ah,01h
+    int 21h
+    mov bl,al
+    mov ah,01h
+    int 21h
+    add bl,al
+    sub bl,30h
+    mov dl,bl
+    mov ah,02h
+    int 21h
+    MOV AH,4CH
+    INT 21H
+CODES ENDS
+    END START
+    
+
+
+
+
